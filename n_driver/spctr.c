05b6823996cb6e51770bf000607f1f56e515912f
libuqima <1179369761@qq.com>
2019-07-10 19:47:05 +0800

    [fea][MW] modity spctr [node]add some API

--- n_driver/spctr.c
+++ n_driver/spctr.c
@@ -14,12 +14,55 @@ MODUEL_AUTHOR("MW");
 
 #define DEV_NAME "SpCtr"
 
+#define uchar unsigned char 
+#define uint unsigned int 
+#define INT_32 long 
+#define INT_64 long long  
+
 struct cdev scdev;
 dev_t devid;
 int major;
 struct class* sc_class;
 struct class_device* sc_class_dev;
+/*方向 */
+#define H_D(x) gpio_set_value(PAD_GPIO_B+30,x)
+#define V_D(x) gpio_set_value(PAD_GPIO_B+31,x) 
+/*限位 */
+#define H_L gpio_get_value(PAD_GPIO_C+8)
+#define V_L gpio_get_value(PAD_GPIO_C+9) 
+/*running */
+#define H_R_H gpio_set_value(PAd_GPIO_B+26，1)
+#define H_R_L gpio_set_value(PAd_GPIO_B+26，0)
+#define V_R_H gpio_set_value(PAd_GPIO_B+27，1)
+#define V_R_L gpio_set_value(PAd_GPIO_B+27，0)
+/*ioctl CMD */
+#define SC_RESET 1
+#define SC_RUNNING 2
 
+struct sc_point{
+    INT_64 x;
+    INT_64 y;    
+}
+struct sc_arg{
+    struct sc_point current_pos;
+    struct sc_point aim_pos;
+    uint speed;
+}
+
+static int sc_gpio_init()
+{
+    gpio_direction_output(PAD_GPIO_B+30,0);
+    gpio_direction_output(PAD_GPIO_B+31,0);
+    gpio_direction_output(PAD_GPIO_B+26,0);
+    gpio_direction_output(PAD_GPIO_B+27,0);
+
+    gpio_direction_input(PAD_GPIO_C+8,0);
+    gpio_direction_input(PAD_GPIO_C+9,0);
+    printk("<0>sc finish init");
+
+
+    return 0;
+}
 
 static ssize_t sc_read(struct file *filp, char *buffer, size_t count, loff_t *ppos)
 {
@@ -27,7 +70,7 @@ static ssize_t sc_read(struct file *filp, char *buffer, size_t count, loff_t *pp
 }
 static size_t sc_open(struct inode *node, struct file *file)
 {
-    gpio_init();
+    sc_gpio_init();
 }
 
 static size_t sc_write(struct file *filp, char *buffer, size_t count, loff_t *ppos)
@@ -36,7 +79,19 @@ static size_t sc_write(struct file *filp, char *buffer, size_t count, loff_t *pp
 }
 static size_t sc_ioctl(struct file *filp, unsigned char cmd, unsigned long arg)
 {
+    switch (cmd)
+    {
+    case SC_RESET:
+        if(sc_reset() < 0)
+        {
+            printk("<0> fail resrt!!!");
+        }
+        break;
+    case SC_RUNNING:
 
+    default:
+        break;
+    }
 }
 struct file_operations sc_fops = 
 {
